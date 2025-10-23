8973f13eec97d38513c149df2db03ce78be817ba
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-23 18:06:20 -0300

    ♻️ refactor: simplifica Navbar usando DrawerMenu e UserMenu
    Refatora o componente App da Navbar para utilizar os novos componentes DrawerMenu e UserMenu, removendo lógica duplicada de menu e drawer, melhorando organização e legibilidade.

--- react-navbar/src/App.tsx
+++ react-navbar/src/App.tsx
@@ -1,172 +1,32 @@
 import { useEffect, useState } from 'react'
-import {
-    AppBar,
-    Badge,
-    Box,
-    Button,
-    Divider,
-    Drawer,
-    IconButton,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
-    Menu,
-    MenuItem,
-    Toolbar,
-    Typography,
-} from '@mui/material'
-import {
-    checkIsAuthenticated,
-    logoutFunction,
-} from '../../utils/src/homehub-utils'
+import { AppBar, Box, Button, Drawer, IconButton, Toolbar } from '@mui/material'
+import { AccountCircle, Notifications } from '@mui/icons-material'
+import { checkIsAuthenticated } from '../../utils/src/homehub-utils'
 import { AuthInfo } from '../../utils/src/types/AuthInfo'
-import AccountCircle from '@mui/icons-material/AccountCircle'
-import NotificationsIcon from '@mui/icons-material/Notifications'
-import SettingsIcon from '@mui/icons-material/Settings'
-import PersonIcon from '@mui/icons-material/Person'
-import LogoutIcon from '@mui/icons-material/Logout'
-import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard'
-import WifiIcon from '@mui/icons-material/Wifi'
-import LockIcon from '@mui/icons-material/Lock'
+import DrawerMenu from './components/DrawerMenu'
+import UserMenu from './components/UserMenu'
 import HomeHubLogo from './assets/home-hub.png'
 
 const App = () => {
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const [drawerOpen, setDrawerOpen] = useState<boolean>(false)
     const [authInfo, setAuthInfo] = useState<AuthInfo>()
 
     useEffect(() => {
         const { authInfo: authObj, isAuthenticated } = checkIsAuthenticated()
 
         if (!isAuthenticated) {
-            location.replace('/')
-            return
+            return location.replace('/')
         }
 
         setAuthInfo(authObj)
     }, [])
 
-    const isMenuOpen = Boolean(anchorEl)
-
-    const [open, setOpen] = useState<boolean>(false)
-
-    const toggleDrawer = (newOpen: boolean) => () => {
-        setOpen(newOpen)
-    }
-
-    const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    const openMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget)
     }
 
-    const handleMenuClose = () => {
-        setAnchorEl(null)
-    }
-
-    const DrawerList = (
-        <Box
-            sx={{ width: 250 }}
-            role="presentation"
-            onClick={toggleDrawer(false)}
-        >
-            <List>
-                <ListItem
-                    disablePadding
-                    onClick={() =>
-                        location.replace(`/dashboard/${authInfo.authId}/`)
-                    }
-                >
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <SpaceDashboardIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Visão geral" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <WifiIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Dispositivos" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <LockIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Segurança" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <SettingsIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Configurações" />
-                    </ListItemButton>
-                </ListItem>
-            </List>
-        </Box>
-    )
-
-    const renderMenu = (
-        <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-            }}
-            keepMounted
-            transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-            }}
-            open={isMenuOpen}
-            onClose={handleMenuClose}
-        >
-            <MenuItem
-                onClick={handleMenuClose}
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'flex-start',
-                }}
-            >
-                <Typography>{`${authInfo?.firstName} ${authInfo?.lastName}`}</Typography>
-                <Typography variant="body2" sx={{ color: '#00000099' }}>
-                    {authInfo?.email}
-                </Typography>
-            </MenuItem>
-            <Divider />
-            <MenuItem onClick={handleMenuClose}>
-                <ListItemIcon>
-                    <SettingsIcon />
-                </ListItemIcon>
-                <ListItemText>Configurações</ListItemText>
-            </MenuItem>
-            <MenuItem
-                onClick={() =>
-                    location.replace(
-                        `/dashboard/${authInfo.authId}/edit-profile`
-                    )
-                }
-            >
-                <ListItemIcon>
-                    <PersonIcon />
-                </ListItemIcon>
-                <ListItemText>Perfil</ListItemText>
-            </MenuItem>
-            <Divider />
-            <MenuItem onClick={logoutFunction}>
-                <ListItemIcon>
-                    <LogoutIcon />
-                </ListItemIcon>
-                <ListItemText>Sair</ListItemText>
-            </MenuItem>
-        </Menu>
-    )
+    const closeMenu = () => setAnchorEl(null)
 
     return (
         <div id="single-spa-application:react-navbar">
@@ -175,7 +35,7 @@ const App = () => {
                     <Toolbar>
                         <Button
                             sx={{ padding: 0 }}
-                            onClick={toggleDrawer(true)}
+                            onClick={() => setDrawerOpen(true)}
                         >
                             <img
                                 src={HomeHubLogo}
@@ -187,26 +47,28 @@ const App = () => {
                         <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
                             <IconButton
                                 size="large"
-                                edge="end"
-                                aria-label="account of current user"
-                                aria-haspopup="true"
-                                onClick={handleProfileMenuOpen}
                                 color="inherit"
+                                onClick={openMenu}
                             >
                                 <AccountCircle />
                             </IconButton>
                             <IconButton size="large" color="inherit">
-                                <Badge color="error">
-                                    <NotificationsIcon />
-                                </Badge>
+                                <Notifications />
                             </IconButton>
                         </Box>
                     </Toolbar>
                 </AppBar>
-                <Drawer open={open} onClose={toggleDrawer(false)}>
-                    {DrawerList}
+                <Drawer open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+                    <DrawerMenu
+                        authId={authInfo?.authId}
+                        onClose={() => setDrawerOpen(false)}
+                    />
                 </Drawer>
-                {renderMenu}
+                <UserMenu
+                    anchorEl={anchorEl}
+                    authInfo={authInfo}
+                    onClose={closeMenu}
+                />
             </Box>
         </div>
     )
