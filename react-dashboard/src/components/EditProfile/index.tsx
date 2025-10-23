a541a54a730c5bd3d5b5e98e59261b833baf67fe
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-23 14:08:17 -0300

    ✨ feat: cria componente EditProfile com formulário de edição de perfil
    Adiciona o componente EditProfile com campos de email, nome e sobrenome, utilizando react-hook-form e componentes do Material UI para validação e envio do formulário.

---
+++ react-dashboard/src/components/EditProfile/index.tsx
@@ -0,0 +1,72 @@
+import { useForm } from 'react-hook-form'
+import { Box, Button, TextField } from '@mui/material'
+
+type FormData = {
+    email: string
+    password: string
+}
+
+const EditProfile = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormData>()
+
+    const onSubmit = (data: FormData) => console.log(data)
+
+    return (
+        <Box
+            component="form"
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 2,
+                py: 2,
+                width: '100%',
+            }}
+            onSubmit={handleSubmit(onSubmit)}
+        >
+            <TextField
+                id="email"
+                label="Email"
+                variant="standard"
+                error={!!errors.email}
+                helperText={errors.email?.message}
+                {...register('email', {
+                    required: 'O e-mail é obrigatório.',
+                    pattern: {
+                        value: /\S+@\S+\.\S+/,
+                        message: 'E-mail inválido.',
+                    },
+                })}
+            />
+            <TextField
+                id="firstName"
+                label="Nome"
+                variant="standard"
+                error={!!errors.password}
+                helperText={errors.password?.message}
+                {...register('password')}
+            />
+            <TextField
+                id="lastName"
+                label="Sobrenome"
+                variant="standard"
+                error={!!errors.password}
+                helperText={errors.password?.message}
+                {...register('password')}
+            />
+            <Button
+                variant="contained"
+                size="large"
+                type="submit"
+                sx={{ bgcolor: '#9C27B0' }}
+            >
+                Editar Perfil
+            </Button>
+        </Box>
+    )
+}
+
+export default EditProfile
